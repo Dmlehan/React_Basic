7a89219577d77d69fa5162c6c83c6d31668bac1c
Dmlehan <dmlehanamanda@gmail.com>
2025-10-22 17:00:45 +0530

    adding setters and getters method

---
+++ Spread/JS_Classes/Getter _and_Setters.js
@@ -0,0 +1,28 @@
+class Square{
+    constructor(_width){
+        this.width=_width;
+        this.height=_height
+    }
+     getArea(){
+        console.log(this.width,this.height);
+        return this.width*this.height;
+    }
+    get area(){
+        return this.width*this.height;
+    }
+    set area(newArea){
+        this.width=Math.sqrt(newArea);
+        this.height=Math.sqrt(newArea);
+    }
+    
+
+}
+const newSquare1=new Square(10);
+
+// const newSquare=new Square(10);
+// console.log(newSquare.getArea()); 
+
+const newSquare=new Square(10);
+console.log(newSquare.area);
+newSquare.area=25;
+console.log(newSquare.area);
