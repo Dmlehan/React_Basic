36fc4a8a9125d5ff29f471ec7622a34be112165e
Dmlehan <dmlehanamanda@gmail.com>
2025-10-23 12:12:31 +0530

    inherit

---
+++ Spread/JS_Classes/Inheritedclass.js
@@ -0,0 +1,22 @@
+class person{
+    constructor(_name,_age){
+         this.name=_name;
+         this.age=_age;
+         console.log('A new person is Created');
+    }
+}
+
+class programmer extends person{
+  constructor(_name,_age_language){
+    super(_name,_age)
+      this.name=_name;
+      this.age=_age;
+      this.language=_language;
+  }
+  code(){
+    return '${this.name} is programmer & knows ${this.language}';
+  }
+}
+const join=new person('john','32');
+const join2=new person('smith','25');
+const jane1=new programmer();
