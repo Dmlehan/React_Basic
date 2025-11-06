f5feafab0548da66485318e984e781551a085747
Dmlehan <dmlehanamanda@gmail.com>
2025-11-06 11:10:25 +0530

    call back hell(reason is to create promises )

--- T-09- Promises/callBack.hell.js
+++ T-09- Promises/callBack.hell.js
@@ -1,17 +1,46 @@
-
-function openFile(callback){
-setTimeout(()=>{
-    console.log('File Opened...');
-    callback();
-},3000);
+function func1(callback){
+   setTimeout(()=>{
+      console.log('This is func1');
+       callback();
+   },1000);
 }
 
-function addSomething(){
-    console.log('Something Added to the File...');
+function func2(callback){
+   setTimeout(()=>{
+      console.log('This is func2');
+       callback();
+   },1000);
 }
 
-function removeSomwthing (){
-    console.log('Something Removed from the File...');
+function func3(callback){
+   setTimeout(()=>{
+      console.log('This is func3');
+       callback();
+   },1000);
 }
-openFile(removeSomwthing);
-addSomething(addSomething);
+
+function func4(callback){
+   setTimeout(()=>{
+      console.log('This is func4');
+       callback();
+   },1000);
+}
+
+function func5(callback){
+   setTimeout(()=>{
+      console.log('This is func5');
+       callback();
+   },1000);
+}
+
+func1(()=>{
+    func2(()=>{
+        func3(()=>{
+            func4(()=>{
+                func5(()={
+                    console.log('All done');
+                });
+            });
+        });
+    });
+});
