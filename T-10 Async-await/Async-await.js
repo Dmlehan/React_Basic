66a31013315b95db2472404a266bab6d5051e126
Dmlehan <dmlehanamanda@gmail.com>
2025-11-06 14:53:15 +0530

    asyn and await keyword instead of the then keywor

---
+++ T-10 Async-await/Async-await.js
@@ -0,0 +1,52 @@
+function takeShower(){
+  return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+            console.log('you take a shower')
+            const shower=true;
+            if(shower){
+            resolve('You take shower');
+            }else{
+             reject('you didnt take shower');
+            }
+         },3000);
+  });
+}
+
+function doHomework(){
+  return new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+      console.log('You do your homework');
+      const homeWork=false;
+      if(homeWork){
+           resolve('you completed your homework');
+      }else{
+           reject('you didnt complete homework');
+      }
+    
+     },2000);
+  });
+}
+function play(){
+       return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+             console.log('you play what you like');
+             const play=true;
+             if(play){
+             resolve('you played what you like');
+             }else{
+             reject('you didnt play');
+             }
+          },1000); 
+       });
+}
+
+async function doAll() {
+    try{
+     await takeShower();
+     await doHomework();
+     await play();
+    }catch(error){
+        console.log(error);
+    }
+}
+doAll();
