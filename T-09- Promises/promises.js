d50a51f765e0e6c5921fb8e428634c03c9d448a0
Dmlehan <dmlehanamanda@gmail.com>
2025-11-06 14:07:22 +0530

    synchronizsynchronously calling function by using promises and then keyword

--- T-09- Promises/promises.js
+++ T-09- Promises/promises.js
@@ -2,16 +2,39 @@ function takeShower(){
   return new Promise((resolve,reject)=>{
          setTimeout(()=>{
             const shower=true;
-            if(shower){
                 console.log('you take a shower')
-                resolve
-            }
+                resolve('You take shower');
          },3000);
   });
 }
-function doHomework(){
 
+function doHomework(){
+  return new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+      console.log('You do your homework');
+      resolve('you completed your homework');
+     },2000);
+  });
 }
 function play(){
-
+       return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+             console.log('you play what you like');
+             resolve('you played what you like');
+          },1000); 
+       });
 }
+
+takeShower()
+.then((result)=>{
+ console.log(result);
+ return doHomework();
+})
+.then((result)=>{
+ console.log(result);
+  return play();
+})
+.then((result)=>{
+   console.log(result);
+  console.log('All done');
+});
