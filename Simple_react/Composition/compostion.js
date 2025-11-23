24a7197d9b8291c3538db941e0f5cfba3336b79b
Dmlehan <dmlehanamanda@gmail.com>
2025-11-23 10:57:16 +0530

    js composition

--- Simple_react/Composition/compostion.js
+++ Simple_react/Composition/compostion.js
@@ -22,5 +22,25 @@ console.log(add3(5))
 y=substract1(9);
 console.log(y);
 
-y=substract1(multiplyBy3(add2(x)));
+y=substract1(multiplyBy3(add2(5)));
 console.log(y);
+
+
+
+function compose(){
+
+    var functions = arguments;
+
+    return function (input){
+       var value =input;
+
+       for(var i=0; i<=functions.length;i++){
+        value=functions[i](value);
+       }
+       return value;
+    }
+}
+
+var allInOneFunction=compose(add2,multiplyBy3,substract1);
+var result=allInOneFunction(18);
+console.log(result);
