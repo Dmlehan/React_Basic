64e04ce98bc61a619ed9386af9374b17b3482d37
Dmlehan <dmlehanamanda@gmail.com>
2025-11-25 09:32:25 +0530

    react first app with simple components

--- T-17-Pure React Component/App.js
+++ T-17-Pure React Component/App.js
@@ -6,32 +6,31 @@
      ]);
   };
 
-  
-//   const Student=()=>{
-//      return React.createElement("div",{},[
-//          React.createElement("h1",{},""),
+  const StudentCard = () =>{
+     return React.createElement(
+          "div",{
+            className:"st name"
+          },
+           React.createElement("img",
+                  { src:"https://i.pinimg.com/1200x/2c/45/bd/2c45bdac6ce0841e6290276e2efd7da5.jpg",
+                    alt:"Student-Image",
+                    className:"studnet-image"
+                  
+                  }),
+                       React.createElement(Student)
+     );
 
-//      ]);
-//   };
-  
+  }
   
   const App=()=>{
-            // 1
-            //////////////////////////////////////////////
              return React.createElement(
-            //   2
                 "div",
                 {},
-             ////////////////////////////////////////////////   
               [ 
-                React.createElement("hi",{},"My react App"),
-                // "This is  React App";
-                React.createElement(Student)
-              ]
-              //////////////////////////////////////////////
-               // 2?
-             );
-            // 1
+               React.createElement("hi",{},"My react App"),
+               React.createElement(StudentCard),
+               React.createElement(StudentCard)
+               ]);
              };
 
           const container=document.getElementById("root");
